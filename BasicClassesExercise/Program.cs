645a078cf1c032bbb81a12fa3385b89da22016ea
Jake Lane <lanejake13@gmail.com>
2020-11-03 14:31:28 -0500

    finished exercise

---
+++ BasicClassesExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Mustang";
+            myCar.Year = 2012;
+
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = 2016
+            };
+
+            var chevy = new Car("Chevy", "Impala", 2001);
+
+            var carList = new List<Car> { myCar, mazda, chevy };
+
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+        }
+    }
+}
